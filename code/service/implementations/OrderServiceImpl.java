a8cd3f64f2584faa0bb6c57e7618d383ff0276ac
jovanvucetic <vuceticjovan1@gmail.com>
2023-03-28 18:46:53 +0200

    Added constant for default delivery fee

--- code/service/implementations/OrderServiceImpl.java
+++ code/service/implementations/OrderServiceImpl.java
@@ -16,6 +16,7 @@ import core.models.*;
 
 public class OrderServiceImpl implements OrderService {
 
+	private final double DEFAULT_DELIVERY_FEE = 199.99;
     private DeliveryRepository deliveryRepository;
     private PaymentRepository paymentRepository;
     private RestaurantRepository restaurantRepository;
@@ -56,7 +57,7 @@ public class OrderServiceImpl implements OrderService {
                         }
 
                         //Adding delivery fee
-                        fullOrderPrice = fullOrderPrice.add(new BigDecimal(199.99));
+                        fullOrderPrice = fullOrderPrice.add(new BigDecimal(DEFAULT_DELIVERY_FEE));
 
                         if (orderDetails.getPaymentDetails().getPaymentType() == PaymentType.CREDIT_CARD)
                         {
@@ -85,7 +86,7 @@ public class OrderServiceImpl implements OrderService {
                             var orderReportItem = orderItem.getCount() + " x, " + orderItem.getName() + " - " +fullPrice + " din\n";
                             reportStringBuilder.append(orderReportItem);
                         }
-                        reportStringBuilder.append("Delivery price: 199.99M");
+                        reportStringBuilder.append("Delivery price:" + DEFAULT_DELIVERY_FEE);
 
                         Date orderAcceptedOn = new Date();
                         return new AcceptedOrderDetails(orderId, orderAcceptedOn, orderExpectedOn, reportStringBuilder.toString());
